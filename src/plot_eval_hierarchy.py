9bc749e1f7d8fff5efb0de2a96add96a2b8051d3
Amanda Clark <adc0032@auburn.edu>
2025-08-22 10:28:40 -0500

    clean up param effect plot

--- src/plot_eval_hierarchy.py
+++ src/plot_eval_hierarchy.py
@@ -193,7 +193,8 @@ def create_parameter_effects(data: pd.DataFrame, varying_params: List[str], avai
                            yerr=param_stats['std'], 
                            marker='o', color='blue', linewidth=2, markersize=6)
             
-            ax.set_xlabel(param.replace('_', ' ').title(), fontweight='bold')
+            if param_idx == 2:
+                ax.set_xlabel(param.replace('_', ' ').title(), fontweight='bold')
             ax.set_ylim(0, y_max)
             # Y-axis labels
             if param_idx == 0:  # Only leftmost column
